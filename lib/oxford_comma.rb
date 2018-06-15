acb59c7bc42caa1ca8ede33ffc4860301ddc6891
Christofer Chan <devchumbing@gmail.com>
2018-06-15 21:05:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    array.join(", ")
+    deleted = array.delete_at(-1)
+    array.join(", ") << " and #{deleted}"
   end
 end
